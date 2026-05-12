1fb53702baf29152f1e534b9e6bdbc1985ebfa41
iwate <github@iwate.me>
2026-05-12 22:36:55 +0900

    Add initial flake.nix configuration for SRT server

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A SRT server for v4l2loopback devices";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
